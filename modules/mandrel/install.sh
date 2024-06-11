d9eab8fa5b35371bbed6b4745464388193df36d2
cgruver <cgruver@clg.lab>
2024-06-11 12:23:48 +0000

    update for JDK specific Mandrel

--- modules/mandrel/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-TEMP_DIR="$(mktemp -d)"
-mkdir -p /usr/local/graalvm 
-curl -fsSL -o ${TEMP_DIR}/mandrel-java17-linux-amd64-${MANDREL_VERSION}.tar.gz https://github.com/graalvm/mandrel/releases/download/mandrel-${MANDREL_VERSION}/mandrel-java17-linux-amd64-${MANDREL_VERSION}.tar.gz 
-tar xzf ${TEMP_DIR}/mandrel-java17-linux-amd64-${MANDREL_VERSION}.tar.gz -C /usr/local/graalvm --strip-components=1 
-rm -rf "${TEMP_DIR}"
