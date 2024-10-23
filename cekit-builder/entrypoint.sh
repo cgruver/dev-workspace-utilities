fd4c91fd83aaa0b0a13b46126b7a54ec175ce995
cgruver <cgruver@clg.lab>
2024-10-23 14:11:54 +0000

    update for fuse-overlayfs

--- cekit-builder/entrypoint.sh
+++ cekit-builder/entrypoint.sh
@@ -5,6 +5,12 @@ then
   mkdir -p "${HOME}"
 fi
 
+if [ ! -d "${HOME}/.config/containers" ]
+then
+  mkdir -p ${HOME}/.config/containers
+  (echo '[storage]';echo 'driver = "overlay"';echo 'graphroot = "/tmp/graphroot"';echo '[storage.options.overlay]';echo 'mount_program = "/usr/bin/fuse-overlayfs"') > ${HOME}/.config/containers/storage.conf
+fi
+
 if ! whoami &> /dev/null
 then
   if [ -w /etc/passwd ]
